261af68863222450940b55e485c0d7f7056b905c
Carverook <45373264+Carverook@users.noreply.github.com>
2018-11-29 20:25:50 +0200

    Update profile_api.php

--- api/profile_api.php
+++ api/profile_api.php
@@ -1,16 +1,12 @@
 <?php
 $db     = new SafeMysql();
-$t = $_GET['t'];
-if($t == "info")
-{
-    if (isset($_POST['token'])) {
-        $token = $_POST['token'];
-        $row = $db->getRow("SELECT * FROM `token` WHERE `token`=?s", $token);
-        if($row['token'] != "") {
-            $id_account = $row['id'];
-            $row = $db->getRow("SELECT * FROM `profile` WHERE `id`=?i", $id_account);
-            if($row['id'] != "")
-            {
+if (isset($_POST['token'])) {
+    $token = $_POST['token'];
+    $id_account_sql = $db->getRow("SELECT * FROM `token` WHERE `token`=?s", $token);
+    $id_account = $id_account_sql['id'];
+    $t = $_GET['t'];
+    if ($t == "info") {
+            if ($row = $db->getRow("SELECT * FROM `profile` WHERE `id`=?i", $id_account)) {
                 $result = array(
                     "Status" => "200",
                     "id" => $row['id'],
@@ -20,39 +16,20 @@ if($t == "info")
                     "type" => $row['type']
                 );
                 echo json_encode($result);
-            }
-            else
-            {
+            } else {
                 $result = array(
                     "Status" => "404"
                 );
 
                 echo json_encode($result);
             }
-        }
-        else
-        {
-            $result = array(
-                "Status" => "404"
-            );
-
-            echo json_encode($result);
-        }
-    }
-}
-else if($t == "update")
-{
-    if (isset($_POST['token'])) {
-        $token = $_POST['token'];
-        $row = $db->getRow("SELECT * FROM `token` WHERE `token`=?s", $token);
-        $id_account = $row['id'];
+    } else if ($t == "update") {
         $check = $db->getRow("SELECT * FROM `profile` WHERE `id`=?i", $id_account);
-        if($check['id'] != 0)
-        {
+        if ($check['id'] != 0) {
             if (isset($_POST['photo'])) {
-            $photo = $_POST['photo'];
-            $db->query("UPDATE `profile` SET `photo` = ?s WHERE `id`=?i", $photo, $id_account);
-        }
+                $photo = $_POST['photo'];
+                $db->query("UPDATE `profile` SET `photo` = ?s WHERE `id`=?i", $photo, $id_account);
+            }
             if (isset($_POST['name'])) {
                 $name = $_POST['name'];
                 $db->query("UPDATE `profile` SET `name` = ?s WHERE `id`=?i", $name, $id_account);
@@ -61,14 +38,103 @@ else if($t == "update")
                 $number = $_POST['number'];
                 $db->query("UPDATE `profile` SET `number` = ?s WHERE `id`=?i", $number, $id_account);
             }
-        }
-        else
-        {
+            $result = array(
+                "Status" => "200"
+            );
+
+            echo json_encode($result);
+        } else {
             $result = array(
                 "Status" => "404"
             );
 
             echo json_encode($result);
         }
+    } else if ($t == "addlist") {
+        $product = $_POST['product'];
+        if(isset($product) && !empty($product)) {
+            if ($check = $db->getRow("SELECT * FROM `likelist` WHERE `id`=?i AND `product`=?i", $id_account, $product)) {
+                $result = array(
+                    "Status" => "404" //Заменить статус-код на подходящий.
+                );
+
+                echo json_encode($result);
+            } else {
+                $db->query("INSERT INTO `likelist` SET `id`=?i, `product`=?i", $id_account, $product);
+                $result = array(
+                    "Status" => "200"
+                );
+
+                echo json_encode($result);
+            }
+        }
+        else
+        {
+            $result = array(
+                "Status" => "404" //Заменить статус-код на подходящий.
+            );
+
+            echo json_encode($result);
+        }
+    } else if ($t == "dellist") {
+        $product = $_POST['product'];
+        if (isset($product) && !empty($product)) {
+            if ($check = $db->getRow("SELECT * FROM `likelist` WHERE `id`=?i AND `product`=?i", $id_account, $product)) {
+                $db->query("DELETE FROM `likelist` WHERE `id`=?i AND`product`=?i", $id_account, $product);
+                $result = array(
+                    "Status" => "200"
+                );
+
+                echo json_encode($result);
+            } else {
+                $result = array(
+                    "Status" => "404" //Заменить статус-код на подходящий.
+                );
+
+                echo json_encode($result);
+            }
+        } else {
+            $result = array(
+                "Status" => "404" //Заменить статус-код на подходящий.
+            );
+
+            echo json_encode($result);
+        }
+    } else if($t == "horder") {
+        $product = $_POST['product'];
+        $total = $_POST['total'];
+        $date = $_POST['date'];
+        if (isset($product) && !empty($product)) {
+            if (isset($total) && !empty($total)) {
+                $db->query("INSERT INTO `OrdersHistory` SET `id`=?i, `product`=?s, `total`=?i,`date`=?s", $id_account, $product, $total,$date);
+                $result = array(
+                    "Status" => "200"
+                );
+
+                echo json_encode($result);
+            }
+            else {
+                $result = array(
+                    "Status" => "404" //Заменить статус-код на подходящий.
+                );
+
+                echo json_encode($result);
+            }
+        }
+        else {
+            $result = array(
+                "Status" => "404" //Заменить статус-код на подходящий.
+            );
+
+            echo json_encode($result);
+        }
     }
+}
+else
+{
+    $result = array(
+        "Status" => "404"
+    );
+
+    echo json_encode($result);
 }
